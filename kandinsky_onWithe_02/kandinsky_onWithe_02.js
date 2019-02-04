3a21cd54f21c521e0326ae4c76004c081fc30594
cesarMelo32 <47233067+cesarMelo32@users.noreply.github.com>
2019-02-04 17:29:38 -0500

    Add files via upload

---
+++ kandinsky_onWithe_02/kandinsky_onWithe_02.js
@@ -0,0 +1,123 @@
+function setup() {
+
+ createCanvas(500, 500);
+   
+ noStroke();
+ fill(224,211,8);
+ triangle(22,11,40,50,60,45);
+ 
+ fill(555,0,0);
+ triangle(13,55,28,72,45,59);
+ 
+ fill(555,0,0);
+ ellipse(46,17,5);
+ 
+ 
+ fill(93,78,40);
+ quad(28,60,71,31,110,73,62,105);
+ 
+ fill(33,80,225);
+ quad(37,77,43,70,55,83,37,87);
+ 
+ 
+ fill(228,170,13,120);
+ //stroke(154, 136, 26);
+ quad(60,87,77,72,95,95,62,125);
+ 
+ 
+ fill(224,211,8);
+ arc(53, 67, 10, 10, 0, 330, CHORD);
+ 
+ 
+ fill(0,0,0)
+ //(12,115,67,68,57,68,42,51,39,60,39,55,43,42,63,63,70,56,75,60,130,12,78,62,130,105,76,68,15,120);
+ 
+ beginShape()
+ vertex(12,115);
+ vertex(67,68);
+ vertex(57,68);
+ vertex(42,51);
+ vertex(39,60);
+ vertex(36,55);
+ vertex(43,42);
+ vertex(63,63);
+ vertex(70,56);
+ vertex(75,60);
+ vertex(130,12);
+ vertex(78,62);
+ vertex(130,105);
+ vertex(76,68);
+ vertex(72,68);
+ vertex(15,120);
+  
+ endShape(CLOSE);
+ 
+ 
+ 
+ 
+ quad(94,100,120,115, 120,110,93,93);
+ 
+ 
+ 
+ 
+ 
+ stroke(255,255, 255)
+ strokeWeight(4);
+ curve(94,80,70,88,70,96,94,80);
+ 
+ strokeWeight(2);
+ stroke(255,0,0);
+ point(68,90);
+ point(67,93);
+ point(69,96);
+ 
+ 
+ //puntos "ojos"
+ strokeWeight(5);
+ stroke(255,255,255);
+ point(65,85);
+ 
+ strokeWeight(2);
+ stroke(0,0,0);
+ point(65,85);
+ 
+
+ strokeWeight(5);
+ stroke(255,255,255);
+ point(73,80);
+ 
+ strokeWeight(2);
+ stroke(0,0,0);
+ point(73,80);
+ 
+ 
+ 
+  strokeWeight(5);
+ stroke(255,255,255);
+ point(71,106);
+ 
+ strokeWeight(2);
+ stroke(0,0,0);
+ point(71,106);
+ 
+ 
+ //
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ noFill();
+ stroke(255, 102, 0);
+}
+
+function draw() {
+   
+  
+  
+}
